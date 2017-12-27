2897ef3dc9230e12fa2357bf3dcc1ae238992aa5
mateodaza <mateodaza@outlook.com>
2017-12-26 22:30:05 -0500

    first commit
    yep

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../styles/App.css';
 import '../styles/Navbar.css';
 
 class Navbar extends Component {
-
+  constructor(props){
+    super(props);
+      this.state={
+          clicked: false
+      }
+    }
 myFunction(e){
 	e.preventDefault();
  	var x = document.getElementById("home");
@@ -28,16 +32,20 @@ myFunction(e){
       */
     }
     	
-        <div className="topnav" id="home">
+      <div className="topnav" id="home">
+          <div className="tabIcon">
+            <img alt=" " src='https://i.imgur.com/06vqYgJ.png' width='6.3%' height='6.3%' ></img>
+          </div>
           <div className="iconDiv">
-          <img src='https://rawgit.com/mateodaza/d83f885a899fc47a3ac7b3046dfb7c50/raw/ba7bb2dfe68eb129b240a1751a62aff1bb60c386/quillalogo.svg' width='6.3%' height='6.3%' ></img>
-         </div>
-  			  <a href="#home" className="active">Home</a>
-  			  <a href="#projects"> Projects </a>
-          <a href="#blog"> Blog </a>
-  			  <a href="#about"> About </a>
-  			  <a style={{fontSize:15, leftMargin: 5}} className="icon" onClick={this.myFunction.bind(this)}>&#9776;</a>
-		    </div>
+            <img alt=" " src='https://rawgit.com/mateodaza/d83f885a899fc47a3ac7b3046dfb7c50/raw/ba7bb2dfe68eb129b240a1751a62aff1bb60c386/quillalogo.svg' width='6.3%' height='6.3%' ></img>
+          </div>
+          <div className="itemsDiv">
+            <a href="#home">Home</a>
+            <a href="#projects"> Projects </a>
+            <a href="#blog"> Blog </a>
+            <a href="#about"> About </a>
+          </div>
+      </div>
 
     </div>
     );
